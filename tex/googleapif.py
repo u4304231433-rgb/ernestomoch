368cec2d69b163d4d32063d56ca997fe3ee071b8
u4304231433-rgb <u4304231433@gmail.com>
2025-11-15 19:32:37 +0100

    Add files via upload

--- tex/googleapif.py
+++ tex/googleapif.py
@@ -7,7 +7,7 @@ import json
 
 SERVICE_ACCOUNT_FILE = "../SECRET/service_account.json"
 
-SCOPES = ['https://www.googleapis.com/auth/drive']
+SCOPES = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/spreadsheets']
 
 def list_drive_files():
     creds = service_account.Credentials.from_service_account_file(
@@ -46,6 +46,77 @@ async def download_file(file_id, destination):
     while not done:
         status, done = downloader.next_chunk()
 
+async def ajouter_ligne_sheet(spreadsheet_id, range_target="Dico", nouvelle_ligne=[]):
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    )
+    service = build('sheets', 'v4', credentials=creds)
+
+    body = {
+        'values': [nouvelle_ligne]
+    }
+
+    result = service.spreadsheets().values().append(
+        spreadsheetId=spreadsheet_id,
+        range=range_target+"!A1",
+        valueInputOption='RAW',
+        insertDataOption='INSERT_ROWS',
+        body=body
+    ).execute()
+
+async def modifier_ligne_sheet(spreadsheet_id, ligne, range_target="Dico", nouvelle_ligne=[]):
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    )
+    service = build('sheets', 'v4', credentials=creds)
+
+    body = {
+        'values': [nouvelle_ligne]
+    }
+    service.spreadsheets().values().update(
+        spreadsheetId=spreadsheet_id,
+        range=range_target+f"!A{ligne+2}:C{ligne+2}", # +1 pour la ligne Francais,+1 car ça commence à 0
+        valueInputOption="RAW",
+        body={"values": [nouvelle_ligne]}
+    ).execute()
+
+async def supprimer_ligne_sheet(spreadsheet_id, ligne, range_target="Dico"):
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    )
+    service = build('sheets', 'v4', credentials=creds)
+
+    sheet_metadata = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+    sheet_d = {}
+    for sheet in sheet_metadata["sheets"]:
+        sheet_d[sheet["properties"]["title"]] = sheet["properties"]["sheetId"]
+        
+    sheet_id = sheet_d[range_target]
+    delete_request = {
+        "requests": [
+            {
+                "deleteDimension": {
+                    "range": {
+                        "sheetId": sheet_id,
+                        "dimension": "ROWS",
+                        "startIndex": ligne+1, #éviter la ligne Francais, etc, commence à 0
+                        "endIndex": ligne+2
+                    }
+                }
+            }
+        ]
+    }
+    service.spreadsheets().batchUpdate(
+        spreadsheetId=spreadsheet_id,
+        body=delete_request
+    ).execute()
+
+
 if __name__ == '__main__':
     list_drive_files()
-    download_file("1dhOPKsrHc8yShN8dJpp3eVmPXlZEL88LvCeYT6MJN0Q","ernestien.csv")
+    """ajouter_ligne_sheet(
+        spreadsheet_id="1dhOPKsrHc8yShN8dJpp3eVmPXlZEL88LvCeYT6MJN0Q",
+        range_target="Dico!A1",
+        nouvelle_ligne=["test", "têst", "éthymologie bla bla"]
+    )"""
+    #download_file("1dhOPKsrHc8yShN8dJpp3eVmPXlZEL88LvCeYT6MJN0Q","ernestien.csv")
