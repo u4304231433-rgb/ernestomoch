781977560468007acd327119fe1390b5b956c50b
u4304231433-rgb <u4304231433@gmail.com>
2025-12-02 23:53:23 +0100

    test

--- bot2.py
+++ bot2.py
@@ -375,6 +375,7 @@ async def on_ready():
         specrights.append("[L]")
     for guild in bot.guilds:
         await guild.me.edit(nick=BOT_NAME)
+    log_save(f"[{datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] LOGGING")
     await update_specrights()
     log_save(f"[{datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] OK: {bot.user} connecté à {';'.join([str(guild.id)+'#'+guild.name for guild in bot.guilds])}")
     load_emojis()
