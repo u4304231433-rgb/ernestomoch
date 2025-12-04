cf121057ddf77069b789cbdcbda31cf80980e2e2
u4304231433-rgb <u4304231433@gmail.com>
2025-12-04 13:30:38 +0100

    remove my previous debug logs

--- bot2.py
+++ bot2.py
@@ -1705,14 +1705,8 @@ class PollButton(discord.ui.Button):
                     log_save("2")
                     await error_response(interaction, "Désolé, ce vote est clos...", duration=5)
             elif self.vote_key == "i":
-                log_save("3")
-                try:
-                    embed = await self.get_embed_infos(interaction)
-                    log_save("4")
-                    await interaction.response.send_message(embed=embed, ephemeral=True, allowed_mentions=discord.AllowedMentions(users=False))
-                    log_save("5")
-                except Exception as e:
-                    log_save(str(e))
+                embed = await self.get_embed_infos(interaction)
+                await interaction.response.send_message(embed=embed, ephemeral=True, allowed_mentions=discord.AllowedMentions(users=False))
             else:
                 log_save("4")
                 await error_response(interaction, f"Désolé, vous ne pouvez voter que si vous étiez citoyen au début du vote. {self.poll_view.citoyens}", duration=20)
